08555f00083aa6d32e22968a825fb84fc0c0289e
Unknown <oleshkevich19990802@mail.ru>
2019-09-29 18:47:50 +0300

    task 2

--- task2.py
+++ task2.py
@@ -1,40 +1,40 @@
 import matplotlib.pyplot as plt
+import math
 import numpy as np
-
+import copy as c
 
 def add_noise(img, rate=5):
-    img[::rate, ::rate, :] = 1
-    return
+    img[::rate, ::rate, :] = 1;
 
+def Kernel(sigma, radius):
+    sigma = 2 * sigma * sigma;
+    kernel = np.zeros((3, 3));
+    for i in range(-radius, radius + 1):
+        for j in range(-radius, radius + 1):
+            kernel[i + radius][j + radius] = math.exp(-(i * i + j * j) / sigma) / math.sqrt(sigma * math.pi);
+    return kernel;
 
-def get_kernel(windos_size):
-    kernel = np.ones((windos_size, windos_size))
-    kernel/=kernel.sum()
-    return kernel
+def FilterByGauss(img):
+    img2 = np.zeros_like(img);
+    radius = 1;
+    kernel = Kernel(5, radius);
 
-
-def filter(img, window_size=3):
-    img2 = np.zeros_like(img)
-    kernel = get_kernel(window_size)
-    p = window_size//2
     for k in range(img.shape[2]): # foreach color channel
-        for i in range(p, img.shape[0]-p): # foreach row
-            for j in range(p, img.shape[1]-p): # foreach column
-                window = img[i-p:i+p+1, j-p:j+p+1, k]
-                img2[i,j,k] = (kernel*window).sum()
-    return img2
-
+        for i in range(radius, img.shape[0] - radius): # foreach row
+            for j in range(radius, img.shape[1] - radius): # foreach column
+                window = img[i - radius:i + radius + 1, j - radius : j + radius + 1, k];
+                img2[i,j,k] = (kernel * window).sum();
+    return img2;
 
 def main():
-    img = plt.imread("img.png")[:, :, :3]
-    add_noise(img)
-    img2 = filter(img)
-
-    fig, axs = plt.subplots(1,2)
-    axs[0].imshow(img)
-    axs[1].imshow(img2)
-    plt.show()
-
+    img = plt.imread("shava.jpg")[:, :, :3];
+    img = c.copy(img);
+    add_noise(img);
+    img2 = FilterByGauss(img);
+    fig, axs = plt.subplots(1,2);
+    axs[0].imshow(img);
+    axs[1].imshow(img2);
+    plt.show();
 
 if __name__ == "__main__":
-    main()
+    main();
